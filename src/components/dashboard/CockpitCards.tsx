ccb15fb42676a82b95f85bf543af11a0a71732b6
Gabriel <gabrielschimtez@gmail.com>
2025-12-12 22:16:18 -0300

    [dyad] Ajustando a formatação de moeda para BRL no Cockpit - wrote 1 file(s)

--- src/components/dashboard/CockpitCards.tsx
+++ src/components/dashboard/CockpitCards.tsx
@@ -27,8 +27,8 @@ export function CockpitCards({ data }: CockpitCardsProps) {
     return new Intl.NumberFormat('pt-BR', {
       style: 'currency',
       currency: 'BRL',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
     }).format(value);
   };
 
